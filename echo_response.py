54aa4efae62531764bde93e3f02106c30f1cb56d
Mikhail C <44262112+konflic@users.noreply.github.com>
2021-05-23 10:03:06 +0300

    Update echo_response.py

--- echo_response.py
+++ echo_response.py
@@ -17,20 +17,13 @@ connection, address = my_socket.accept()
 
 con_data = connection.recv(4096)
 
-print(con_data)
-
-data_from_client = con_data.decode("utf-8")
-headers = data_from_client.split("\n")
+raw_headers = con_data.decode("utf-8").split("\n")
 
 response_data = "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\n\r\n<h2>Got headers:</h2>\r\n"
 
-for header in headers:
+for header in raw_headers:
     if ":" in header:
         response_data += f"<p>{header}</p>"
-
-response_data = response_data
-
-response_data = f"{response_data}"
 
 connection.send(response_data.encode("utf-8"))
 
