2ccbfa8911a30cac289ee20e33aa00b74c525efc
Mikhail C <44262112+konflic@users.noreply.github.com>
2021-07-31 12:42:30 +0300

    Update seasons.py

--- seasons.py
+++ seasons.py
@@ -1,12 +1,12 @@
-season = input("Какое сейчас время года? ")
+season = input("Какое сейчас время года?")
 
-if season == "лето":
+if season == "лето" or season == "Лето":
     print("Солнце светит ярче чем когда-либо")
-elif season == "зима":
+elif season == "зима" or season == "Зима":
     print("За окном падает белый снег")
-elif season == "осень":
+elif season == "осень" or season == "Осень":
     print("Урожай будет невероятным")
-elif season == "весна":
+elif season == "весна" or season == "Весна":
     print("Птицы поют прекрасные песни")
 else:
     print("Требуется ввести правильное название сезона")
